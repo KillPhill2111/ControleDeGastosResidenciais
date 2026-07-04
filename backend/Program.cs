1ac918d7223a9afbfec00ba53c47745da5b673f1
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2026-07-04 10:57:44 -0300

    7º commit:: teste run

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,41 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder=WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+//Configurar banco de dados (SQL lite)
+var connectionString=builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options=>
+    options.UseSqlite(connectionString));
 
-// Configure the HTTP request pipeline.
+
+//Configuração do CORS
+builder.Services.AddCors(options=>
+{
+    options.AddPolicy("Desenvolvimento", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173","http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+
+var app=builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors("Desenvolvimento");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
